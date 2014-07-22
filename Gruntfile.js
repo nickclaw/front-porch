a8ec96fee6da198c5e6d2771900d54901fd4a56b
Nicholas Clawson <nickclaw@gmail.com>
2014-07-21 20:43:37 -0700

    Updated grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,91 @@
 module.exports = function(grunt) {
 
     grunt.initConfig({
-        
+
+        sass: {
+            options: {
+                style: 'compressed',
+                precision: 4,
+            },
+            dist: {
+                files: {
+                    'build/style/main.css': 'src/style/main.scss'
+                }
+            }
+        },
+
+        uglify: {
+            dist: {
+                options: {
+                    compress: true,
+                    report: 'min',
+                    sourceMap: true,
+                    wrap: true
+                },
+                files: {
+                    'build/script/script.js': ['src/script/**/*.js', 'src/script/**/*.json']
+                }
+            },
+            dev: {
+                options: {
+                    compress: false,
+                    mangle: false,
+                    beautify: true,
+                    report: 'min',
+                    wrap: true
+                },
+                files: {
+                    'build/script/script.js': ['src/script/**/*.js', 'src/script/**/*.json']
+                }
+            }
+        },
+
+        htmlmin: {
+            dist: {
+                options: {
+                    removeComments: true,
+                    collapseWhitespace: true
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'src/template',
+                    src: '**/*.html',
+                    dest: 'build/template/'
+                }]
+            }
+        },
+
+        imagemin: {
+            // todo, not important
+        },
+
+        watch: {
+            js: {
+                files: ["src/script/**/*.js"],
+                tasks: ['uglify:dev']
+            },
+            sass: {
+                files: ["src/style/**/*.scss"],
+                tasks: ["sass"]
+            },
+            html: {
+                files: ["src/template/**/*.html"],
+                tasks: ["htmlmin"]
+            },
+            imagemin: {
+                files: [/* todo */],
+                tasks: ["imagemin"]
+            }
+        },
+
+        concurrent: {
+            dev: ['watch:js', 'watch:sass', 'watch:html', 'watch:imagemin'],
+            dist: ['sass', 'htmlmin', 'uglify:dist'],
+            options: {
+                logConcurrentOutput: true,
+                limit: 5
+            }
+        }
     });
 
     grunt.loadNpmTasks('grunt-contrib-sass');
@@ -11,6 +95,6 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-htmlmin');
     grunt.loadNpmTasks('grunt-concurrent');
 
-    grunt.registerTask('default', ['sass']);
-    grunt.registerTask('develop', ['sass', 'watch:sass']);
+    grunt.registerTask('default', ['sass', 'uglify:dev', 'htmlmin', 'concurrent:dev']);
+    grunt.registerTask('build', ['concurrent:dist']);
 };
